46bf23e061a064a3bd92f15822106dbc632fa8d7
mohanhd <mohan.h.d@ibm.com>
2024-05-16 12:38:48 +0530

    customer list is implemented

--- client/src/pages/CustomerList.jsx
+++ client/src/pages/CustomerList.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
 import { getCustomers } from "../lib/graphql/quiries";
+import { Link } from "react-router-dom";
 
 function CustomerList() {
   getCustomers().then((result) => console.log(result));
+  const [customers, setCustomers] = useState([]);
+  useEffect(() => {
+    getCustomers().then(setCustomers);
+  }, []);
   return (
     <>
-      <h2>Customers List</h2>
+      <div>
+        <h1 className="title">Customers</h1>
+        <ul className="box">
+          {customers.map((customer) => (
+            <li className="media">
+              <div className="media-content">
+                <Link to={`/customers/${customer.customerId}`}>
+                  {customer.name}
+                </Link>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
